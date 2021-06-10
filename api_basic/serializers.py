9da8a67459297ac1525e241154945437f1383059
ezytradeAfrica <support@ezytrade.africa>
2021-06-10 20:12:21 +0300

    first commit

---
+++ api_basic/serializers.py
@@ -0,0 +1,12 @@
+from abc import ABC
+
+from rest_framework import serializers
+from .models import Article
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = ['id', 'title', 'author', 'email', 'date']
+
+
